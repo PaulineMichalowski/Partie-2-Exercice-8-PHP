4ce86f6621ccb094f676fa09a7b31b62af5f31e4
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-12 13:46:15 +0100

    Création de l'exercice 8

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <?php
+    $maVariable = null;
+    if($maVariable == null) {
+      echo 'C\'est ok !!';
+    }
+    else {
+      echo 'C\est pas bon !';
+    }
+    ?>
+  </body>
+</html>
